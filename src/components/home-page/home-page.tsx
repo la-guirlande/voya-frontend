be94179dd769e4e948bfe8a69a60bc1c5c5f2bc3
Rocknologie <rocknologie@hotmail.fr>
2021-03-25 17:15:07 +0100

    home-page

---
+++ src/components/home-page/home-page.tsx
@@ -0,0 +1,14 @@
+
+import React, { useContext } from 'react'
+import AuthenticationContext from '../../contexts/authentication-context';
+
+export const HomePage: React.FC = () => {
+
+    const authContext = useContext(AuthenticationContext);
+
+    return (
+        <>
+        </>
+    )
+}
+
