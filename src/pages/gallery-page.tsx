5e746458102f4380fb1ab079d57db06f35b2242e
Miki Namsrai <miki.namsrai@gmail.com>
2021-03-27 23:03:48 +0100

    Add date props

--- src/pages/gallery-page.tsx
+++ src/pages/gallery-page.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import Gallery from '../components/Gallery/gallery'
 
-const GalleryPage = () => {
+interface IGalleryPageProps {
+  city: string;
+  country: string;
+  startDate: Date;
+  endDate: Date
+}
+
+const GalleryPage: React.FC<IGalleryPageProps> = ({ city, country, startDate, endDate }) => {
 
   const images = [
     './img/mountain.jpg',
@@ -11,9 +18,24 @@ const GalleryPage = () => {
     './img/mountain.jpg',
   ];
 
+  const formatDate = (date: Date) => {
+    let parsedDate = date.toLocaleDateString('fr-FR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+    return parsedDate
+  };
+
   return (
-    <div className="bg-black">
-      <Gallery images={images}/>
+    <div className="flex flex-col">
+      <div className="bg-black">
+        <div className="text-white text-2xl text-center pt-4 pb-2 font-montserrat">
+          Votre voyage à {city}, {country}
+        </div>
+        <div className="text-gray-300 text-base text-center pb-4 pt-2 font-montserrat " >
+          Du {formatDate(startDate)} au {formatDate(endDate)}
+        </div>
+        <div className="px-40">
+          <Gallery images={images} />
+        </div>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'quicksand': ['Quicksand', 'sans-serif'],
+        'montserrat': ['Montserrat', 'sans-serif']
+      }
+    },
   },
   variants: {
     extend: {},
