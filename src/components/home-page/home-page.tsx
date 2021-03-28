ee08152a6ee870ac23d9b4935f43b7c0064808aa
Rocknologie <rocknologie@hotmail.fr>
2021-03-28 21:14:43 +0200

    Update Homepage

--- src/components/home-page/home-page.tsx
+++ src/components/home-page/home-page.tsx
@@ -1,13 +1,22 @@
 
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
+import { useHistory } from 'react-router-dom';
 import AuthenticationContext from '../../contexts/authentication-context';
+import { Button } from '../button';
 
 export const HomePage: React.FC = () => {
-
-    const authContext = useContext(AuthenticationContext);
+    const authenticationContext = useContext(AuthenticationContext);
+    const historiqueInternetMDR = useHistory();
+    
+    useEffect(() => {
+        if (!authenticationContext.authUser) {
+            historiqueInternetMDR.push('/login');
+        }
+    }, [authenticationContext.authUser]);
 
     return (
         <>
+            eeeeeeeeeeeee
         </>
     )
 }
